dcbba57d3dd52d4e8eb473a3af137cf33e3a1e0f
daiceza <56700482+daiceza@users.noreply.github.com>
2019-11-06 20:07:12 +0900

    11/06 jQuery課題1-2修正

--- pr01.js
+++ pr01.js
@@ -1,8 +1,8 @@
 var answer = prompt('正の数と負の数を判定します');
-if (answer >=0) {
-  alert('正の数');
-} else if (answer <0) {
-  alert('負の数');
+if (answer === '' || isNaN(answer)) {
+    alert('数値を入力してください');
+} else if (answer >= 0) {
+    alert('正の数');
 } else {
-  alert('数値を入力してください');
+    alert('負の数');
 }
